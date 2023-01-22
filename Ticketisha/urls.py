8ded2e4206e8c494079ee5cf5281d0434bc92f77
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-01-22 17:46:02 +0300

    demo app updated

--- Ticketisha/urls.py
+++ Ticketisha/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("apps.core.urls")),
+    path("accounts/", include("apps.accounts.urls")),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
