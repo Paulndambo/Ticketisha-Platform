bfbb2f946b33a71db6c1f055c4771e4183a5dc13
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2023-01-22 17:16:15 +0300

    templates and models added

---
+++ apps/customers/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+from apps.core.models import AbstractBaseModel
+# Create your models here.
+CUSTOMER_STATUS_CHOICES = (
+    ("active", "Active"),
+    ("disabled", "Disabled"),
+)
+
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=255)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    logo = models.ImageField(upload_to="customer_logos", null=True, blank=True)
+    postal_code = models.CharField(max_length=255, null=True, blank=True)
+    zip_code = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    headquarters = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=255, default="active")
+
+    def __str__(self):
+        return self.name
---
+++ apps/helpdesk/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from apps.customers.models import Customer
+from apps.core.models import AbstractBaseModel
+from django.conf import settings
+# Create your models here.
+TICKET_TYPE_CHOICES = (
+    ("bug", "Bug"),
+    ("feature", "Feature"),
+)
+
+TICKET_STATUS_CHOICES = (
+    ("reported", "Reported"),
+    ("todo", "Todo"),
+    ("in_progress", "In Progress"),
+    ("done", "Done"),
+    ("blocked", "Blocked"),
+)
+
+class Ticket(AbstractBaseModel):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    ticket_type = models.CharField(max_length=255, choices=TICKET_TYPE_CHOICES)
+    description = models.TextField()
+    status = models.CharField(max_length=255, choices=TICKET_STATUS_CHOICES)
+    screenshots = models.ImageField(upload_to="screenshots", null=True, blank=True)
+    reporter = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class TicketDiscussion(AbstractBaseModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    message = models.TextField(blank=True)
+    files = models.FileField(upload_to="ticket_files", null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
