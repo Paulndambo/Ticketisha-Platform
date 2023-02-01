3ec08dc7c645dd2bcd36e88e594c620e7199ee90
Hellen Wainaina <hellenwain@gmail.com>
2023-02-01 22:11:06 +0300

    filter, create, update, delete, view on tickets

--- apps/helpdesk/urls.py
+++ apps/helpdesk/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("open-tickets/", views.open_tickets, name="open-tickets"),
     path("closed-tickets/", views.closed_tickets, name="closed-tickets"),
     path("new-ticket/", views.CreateNewTicket.as_view(), name="new-ticket"),
+    path("ticket-detail/<int:id>/", views.ticket_detail, name="ticket-detail"),
+    path("ticket-update/<int:pk>/", views.UpdateTicket.as_view(), name="update-ticket"),
+    path("ticket-delete/<int:id>/", views.delete_ticket, name="delete-ticket")
 ]
